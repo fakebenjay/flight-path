04fb8960025fa53ec0935f5016bfd145fdbd749d
github username <github email address>
2017-04-17 15:39:58 -0400

    has_secure_pw

--- travel-app-api/app/models/account.rb
+++ travel-app-api/app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
   has_many :trips, through: :planned_trips
   has_many :planned_activities
   has_many :activities, through: :planned_activities
+  has_secure_password
 end
